9892b8b100a0b6d41487b1c0478d852db393e01b
kavindumal <kavindu11250403@gmail.com>
2025-04-02 05:34:57 +0530

    Add animated calendar package with features and installation instructions

--- README.md
+++ README.md
@@ -11,29 +11,17 @@ and the Flutter guide for
 [developing packages and plugins](https://flutter.dev/to/develop-packages). 
 -->
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+# Animated Calendar
+
+A modern, animated calendar package for Flutter with a blue-and-white theme. It provides smooth animations and a beautiful UI.
 
 ## Features
+- Animated date selection
+- Beautiful UI with a blue-and-white theme
+- Month and week view switching
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
-
-```dart
-const like = 'sample';
-```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
+## Installation
+Add this package to your `pubspec.yaml`:
+```yaml
+dependencies:
+  animated_calendar: ^1.0.0
--- lib/src/animated_calendar_base.dart
+++ lib/src/animated_calendar_base.dart
@@ -1,6 +1,130 @@
-// TODO: Put public facing types in this file.
+library animated_calendar;
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class AnimatedCalendar extends StatefulWidget {
+  @override
+  _AnimatedCalendarState createState() => _AnimatedCalendarState();
+}
+
+class _AnimatedCalendarState extends State<AnimatedCalendar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late CalendarFormat _calendarFormat;
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarFormat = CalendarFormat.month;
+    _selectedDay = DateTime.now();
+    _focusedDay = DateTime.now();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Animated Calendar',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Color(0xFF0077B6),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2020, 1, 1),
+              lastDay: DateTime.utc(2030, 12, 31),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  _animationController.forward(from: 0);
+                });
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+              headerStyle: HeaderStyle(
+                formatButtonVisible: true,
+                titleTextStyle:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                decoration: BoxDecoration(
+                  color: Color(0xFF0077B6),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                leftChevronIcon: Icon(Icons.chevron_left, color: Colors.white),
+                rightChevronIcon: Icon(Icons.chevron_right, color: Colors.white),
+              ),
+              calendarStyle: CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  color: Color(0xFF0077B6).withOpacity(0.5),
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Color(0xFF0077B6),
+                  shape: BoxShape.circle,
+                ),
+                todayTextStyle: TextStyle(color: Colors.white),
+                selectedTextStyle: TextStyle(color: Colors.white),
+                defaultTextStyle: TextStyle(color: Colors.black),
+              ),
+            ),
+            SizedBox(height: 20),
+            ScaleTransition(
+              scale: Tween(begin: 0.8, end: 1.0).animate(
+                CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xFF0077B6),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(0.3),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Text(
+                  'Selected: ${DateFormat.yMMMMd().format(_selectedDay)}',
+                  style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
